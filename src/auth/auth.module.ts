b363f556d328a47e28a1ba4a94d7a24ecab745ac
JBLew <autumnlewjb@gmail.com>
2022-04-09 10:42:13 +0800

    integrate firebase google sign in provider
    Use firebase admin sdk to verify the firebase login and authenticate the
    user into the backend. The user will get a jwt token signed in the
    backend. All requests will be using the jwt token, instead of the
    firebase token.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local-auth/local.strategy';
 import { JwtStrategy } from './jwt-auth/jwt.strategy';
-import JwtRefreshGuard from './jwt-refresh/jwt-refresh.guard';
-import { JwtResetPasswordGuard } from './jwt-reset-password/jwt-reset-password.guard';
 import { JwtRefreshStrategy } from './jwt-refresh/jwt-refresh.strategy';
 import { JwtResetPasswordStrategy } from './jwt-reset-password/jwt-reset-password.strategy';
+import { FirebaseAuthStrategy } from './firebase-auth/firebase-auth.strategy';
 
 @Module({
   imports: [
@@ -23,6 +22,7 @@ import { JwtResetPasswordStrategy } from './jwt-reset-password/jwt-reset-passwor
     JwtStrategy,
     JwtRefreshStrategy,
     JwtResetPasswordStrategy,
+    FirebaseAuthStrategy,
   ],
   controllers: [AuthController],
 })
