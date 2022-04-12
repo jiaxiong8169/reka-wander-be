14b85d6ac8d9aadcf2d0bf12aabdc83f5fd908c4
JBLew <autumnlewjb@gmail.com>
2022-04-12 20:39:22 +0800

    use base64 encoded string for firebase private key
    before this implementation, some problem arises when trying to deploy to
    heroku. Firebase SDK failed to parse the private key.
    to use this implementation, the private key in the env var should be
    base64 encoded.

--- src/auth/firebase-auth/firebase-auth.strategy.ts
+++ src/auth/firebase-auth/firebase-auth.strategy.ts
@@ -16,11 +16,18 @@ export class FirebaseAuthStrategy extends PassportStrategy(
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       passReqToCallback: true,
     });
+    const firebase_private_key_b64 = Buffer.from(
+      process.env.FIREBASE_PRIVATE_KEY,
+      'base64',
+    );
+    const firebase_private_key = firebase_private_key_b64
+      .toString('utf8')
+      .replace(/\\n/g, '\n');
     const serviceAccount = {
       type: 'service_account',
       project_id: 'reka-wander',
       private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      private_key: firebase_private_key,
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
       client_id: process.env.FIREBASE_CLIENT_ID,
       auth_uri: 'https://accounts.google.com/o/oauth2/auth',
