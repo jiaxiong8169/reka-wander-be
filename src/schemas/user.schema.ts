6700a8dba469b8f80fe402e7b7a45254f6313ba5
JBLew <autumnlewjb@gmail.com>
2022-06-12 10:53:48 +0800

    Minor change to the user schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -22,7 +22,7 @@ export type UserDocument = User & mongoose.Document;
 export class User {
   _id: mongoose.Schema.Types.ObjectId;
 
-  @Prop({ required: true, default: '' })
+  @Prop({ default: '' })
   name: string;
 
   @Prop({ default: 'user', lowercase: true, required: true, minLength: 1 })
