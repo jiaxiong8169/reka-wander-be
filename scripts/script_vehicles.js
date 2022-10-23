23a8ca2578dea05fe47a3768d92967556dc47b10
Jia Xiong <wing8169@users.noreply.github.com>
2022-10-23 19:46:15 +0800

    Modify database schema script

--- scripts/script_vehicles.js
+++ scripts/script_vehicles.js
@@ -3,5 +3,10 @@ load('./utils.js');
 const data = requireUncached('./data/vehicles.json');
 fixLocationStructure(data);
 
+data.forEach((d) => {
+  // change string into list
+  d.additionalRules = JSON.parse(d.additionalRules.replace(/\\/g, ''));
+});
+
 const result = db.vehicles.insertMany(data);
 print(result);
